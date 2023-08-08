5b1e8e98f5f288db956a347f83273de5dbc51dfc
Ted <andrei.teodor21@yahoo.ro>
2023-08-08 10:51:46 +0200

    Fixed an issue where user was logged back in on page refresh because of not clearing persistence

--- quizbytesclient/src/hooks/useAuth.js
+++ quizbytesclient/src/hooks/useAuth.js
@@ -1,14 +1,21 @@
 import { useContext } from "react";
-import { AuthContext} from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
+import { getAuth, signOut as firebaseSignOut } from "firebase/auth";
 
 function useAuth() {
-    const { auth, setAuth } = useContext(AuthContext);
-  
-    const signOut = () => {
-      setAuth({ token: "", user: null });
-    };
-  
-    return { auth, signOut };
-  }
+  const { auth, setAuth } = useContext(AuthContext);
+
+  const signOut = async () => {
+    const authInstance = getAuth(); // Get the Firebase Auth instance
+    try {
+      await firebaseSignOut(authInstance); // Sign out using Firebase Auth
+      setAuth({ token: "", user: null }); // Clear user in app's context
+    } catch (error) {
+      console.error("Error during sign out:", error);
+    }
+  };
+
+  return { auth, signOut };
+}
 
 export default useAuth;
